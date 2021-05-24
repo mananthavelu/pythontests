80810f2936ba7d4ca9206fa92072e510ba6313d0
Marimuthu Ananthavelu <mari.a.muthu@gmail.com>
2021-05-24 22:58:03 +0200

    Updates:Move element to end

--- arrays.py
+++ arrays.py
@@ -15,6 +15,40 @@ Arrays are great for numerical operations; lists cannot directly handle math ope
     2.2 
     3. Reverse an array
     4. 
+"""
+
+height=1.79
+weight=68.7
+result=weight/height**2
+print(10)
+#Built in functions
+""""
+x=5
+print(type(x))
+y=str(x)
+print(type(y))
+
+#Defining a function
+def square(value):
+    answer=value*value
+    return answer
+
+square_of_4=square(4)
+print(square_of_4)
+
+#loops
+#while
+#for
+#recursion
+
+#Sorting
+
+#Searching
+#Membership
+#Lookin for an element in python
+#Adding an element at the end of the list
+#Adding an element at the beginning of the list
+#Adding an element in a specified position within the list
     
 """
 
---
+++ move_element_to_end_sol1.py
@@ -0,0 +1,13 @@
+# O(n) time | O(1) space - where n is the length of the array
+def moveElementToEnd(array, toMove):
+    i = 0
+    j = len(array) - 1
+    while i < j:
+        while i < j and array[j] == toMove:
+            j -= 1
+        if array[i] == toMove:
+            array[i], array[j] = array[j], array[i]
+        i += 1
+    return array
+
+print(moveElementToEnd([1,4,5,6,7,2,4], 5))
